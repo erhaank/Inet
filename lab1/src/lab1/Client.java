cad72e9b16560e15081cea7e5a1fce11481347ff
mengelin <marwwinn@gmail.com>
2016-01-19 18:48:31 +0100

    Lets do it

---
+++ lab1/src/lab1/Client.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO
+	}
+
+}
---
+++ lab1/src/lab1/Server.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO 
+	}
+
+}
